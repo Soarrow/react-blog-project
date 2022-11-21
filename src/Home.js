e2219019796b82832b58b2779f2fb9e2cb915749
WenBo <wenbo.zhao.1998@gmail.com>
2022-11-21 15:28:53 +0000

    Create custom hook useFetch

--- src/Home.js
+++ src/Home.js
@@ -1,5 +1,5 @@
-import { useState, useEffect } from 'react';
 import BlogList from './BlogList';
+import useFetch from './useFetch';
 
 /**
  * If we want to pass something into a function
@@ -22,43 +22,14 @@ const Home = () => {
     // }
 
     // ACTUAL CODE
-
-    // State initialisations
-    const [blogs, setBlogs] = useState(null);
-    const [isPending, setIsPending] = useState(true);
-    const [error, setError] = useState(null);
+    // semicolon means to use data as blogs ... name the data blogs inside this component ... grab the data but call it blogs ni this context
+    const {data: blogs, isPending, error} = useFetch("http://localhost:8000/blogs");
 
 
     // const [name, setName] = useState('mario');
 
-    // This function fires for every render ... be careful of changing the state inside use effect
-    // empty dependency array causes useEffect hook to only render once
-    // we can specify which state values will trigger the use effect function to run
-    // useEffect is a good place to fetch data because it runs the function when the component runs initially ... genearlly the point where we want to go fetch some data
-    // we can then use that data intead of the data that we have set above in the useState
-    useEffect(() => {
-        setTimeout(() => {
-            fetch('http://localhost:8000/blogs') // returns to us a promise ... then fires a function once the the above has been resolved
-                .then(res => { // when the fetch function resolves itself we get a response object ... this doesn't contain the data it's just a response object
-                    // console.log(res);
-                    if (!res.ok) {
-                        throw Error('could not fetch the data for that resource');
-                    }
-                    return res.json(); // res.json() gets us the data ... this passes the json into a js object for us // this returns another promise as json() is also async
-                })
-                .then((data) => {
-                    console.log(data);
-                    setBlogs(data);
-                    setIsPending(false);
-                    setError(null);
-                })
-                .catch((err) => { // this will catch any kind of network error
-                    // console.log(err.message);
-                    setError(err.message);
-                    setIsPending(false);
-                })
-        }, 1000);
-    }, []);
+
+    
 
     return (
         <div className="home">
