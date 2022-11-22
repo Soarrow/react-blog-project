fb2168e01ca40502a83fde6803a488c628b45e67
WenBo <wenbo.zhao.1998@gmail.com>
2022-11-22 00:15:54 +0000

    Create blog detail page

--- src/BlogDetail.js
+++ src/BlogDetail.js
@@ -1,16 +1,28 @@
 import { useParams } from "react-router-dom";
+import useFetch from "./useFetch";
 
 const BlogDetails = () => {
     /*
         We use a hook from the react router dom to get the route parameter from the url
     */
-    const {id} = useParams();
+    const { id } = useParams();
 
-    return (  
+    const { data: blog, error, isPending } = useFetch('http://localhost:8000/blogs/' + id);
+
+    return (
         <div className="blog-details">
-            <h2>Blog details - {id}</h2>
+            {isPending && <div>Loading...</div>}
+            {error && <div>{error}</div>}
+            {blog && (
+                <article>
+                    <h2>{blog.title}</h2>
+                    <p>Written by {blog.author}</p>
+                    <div>{blog.body}</div>
+                </article>
+            )}
         </div>
+
     );
 }
- 
+
 export default BlogDetails;
