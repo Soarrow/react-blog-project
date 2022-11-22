63ef7a97ac62933acb47be6459e0a05ed47d2f82
WenBo <wenbo.zhao.1998@gmail.com>
2022-11-22 00:57:47 +0000

    implement blog submission functionality

--- src/Create.js
+++ src/Create.js
@@ -1,7 +1,66 @@
+import { useState } from "react";
+
 const Create = () => {
+
+    // Need to associate the below with the value of its corresponding input
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('mario');
+    const [isPending, setIsPending] = useState(false);
+
+    // React to the submit event
+    const handleSubmit = (e) => {
+        // need to prevent the default action (stops page from refreshing)
+        e.preventDefault();
+
+        // create a blog object
+        const blog = {title, body, author};
+
+        setIsPending(true);
+
+        // first arg is the endpoint that we're making the post request to
+        // second arg is where we tack on the data and also define the type of request we're making
+        fetch('http://localhost:8000/blogs', {
+            method: 'POST',
+            headers: {"Content-Type": "application/json"}, //set the content type that is being sent with this request ... we're sending json data
+            body: JSON.stringify(blog) // the data that we're sending with this request ... in this case the blog
+        }).then(() => {
+            console.log('new blog added');
+            setIsPending(false);
+        })
+    }
+
     return (
         <div className="create">
             <h2>Add a New Blog</h2>
+            <form onSubmit={handleSubmit}>
+                <label>Blog title:</label>
+                <input
+                    type="text"
+                    required
+                    value={title}
+                    onChange={(e) => setTitle(e.target.value)}
+                />
+                <label>Blog body:</label>
+                <textarea
+                    required
+                    value={body}
+                    onChange={(e) => setBody(e.target.value)}
+                ></textarea>
+                <label>Blog author:</label>
+                <select
+                    value={author}
+                    onChange={(e) => setAuthor(e.target.value)}
+                >
+                    <option value="mario">mario</option>
+                    <option value="yoshi">yoshi</option>
+                </select>
+                {!isPending && <button>Add Blog</button>}
+                {isPending && <button disabled>Submitting Blog</button>}
+                {/* <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p> */}
+            </form>
         </div>
     );
 }
